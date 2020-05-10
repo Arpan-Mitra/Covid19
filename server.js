c1dd1603ea621294d4336d9297a2db0aeba443c0
Arpan-Mitra <65007418+Arpan-Mitra@users.noreply.github.com>
2020-05-10 23:56:03 +0530

    configure Heroku build

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ ngApp.use(express.static('./dist/covid19-tracker'));
 ngApp.get('/*', function (request, response) {
     response.sendFile(path.join(__dirname, '/dist/covid19-tracker/index.html'));
 });
-ngApp.listen(process.env.PORT || 8080);
+ngApp.listen(process.env.PORT || 8000);
